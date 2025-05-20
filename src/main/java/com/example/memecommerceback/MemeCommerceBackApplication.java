8e4b2c00aa6cb8ab8c1e05e16a8d36b381463027
parkjihwan <mankcom7708@gmail.com>
2025-05-20 22:10:19 +0900

    init commit

---
+++ src/main/java/com/example/memecommerceback/MemeCommerceBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.memecommerceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeCommerceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeCommerceBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/memecommerceback/MemeCommerceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memecommerceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeCommerceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
